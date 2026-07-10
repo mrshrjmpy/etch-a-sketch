14c2be2735c8c2f216763e81b9716e6acb4394f0
aki <mihika.ila@google.com>
2026-07-10 17:11:23 +0530

    working grid w input size

--- javascript.js
+++ javascript.js
@@ -1,14 +1,47 @@
-const cellList = document.querySelectorAll(".col");
-console.log(cellList.length);
-cellList.forEach( tile =>
+addListeners();
+
+function acceptSize(){
+    let size = prompt("Enter value");
+    let i = 0;
+    let j = 0;
+
+    const all_divs = document.querySelector('.alldivs');
+    all_divs.replaceChildren();
+    console.log('all children removed');
+    while (j < size)
+    {
+        const div = document.createElement('div');
+        div.classList.add("container");
+        all_divs.appendChild(div);
+        console.log("row added");
+        j++;
+    }
+    const container_list = document.querySelectorAll(".container");
+
+    console.log(container_list.length);
+    while(i < size)
+    {
+        container_list.forEach(item => {
+            const div = document.createElement('div');
+            div.classList.add("cell");
+            item.appendChild(div);
+            console.log("div added");   
+        });
+        i++;
+    }
+    
+    addListeners();
+}
+
+function addListeners()
 {
-    tile.addEventListener('mouseenter', () => {
-        console.log('hover');
-        tile.style.backgroundColor = '#000000'}
-    );
-    tile.addEventListener('mouseleave', () => {tile.style.backgroundColor = '#d0d0d0'}
+    const cellList = document.querySelectorAll(".cell");
+    cellList.forEach( tile =>
+    {
+        tile.addEventListener('mouseenter', () => {
+            console.log('hover');
+            tile.style.backgroundColor = '#000000'}
+        );
+    }
     );
 }
-);
-
-
